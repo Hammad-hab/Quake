38026a87e5a488d7f80648e029eda5521c2230b9
Hammad-hab <hammadalibutt30@gmail.com>
2026-05-30 13:33:58 +0500

    rm: dist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   appType: "mpa",
-  server: {
-    port: 3000
-  }
+  server: { port: 3000 },
+  plugins: [
+    viteStaticCopy({
+      targets: [{ src: "src/*", dest: "git" }]
+    })
+  ]
 });
